1fe040a44fffaa643862f85fa30e4ea7058a441c
dennyzhang <denny@dennyzhang.com>
2017-09-07 21:41:52 -0500

    minor change

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Python precheck package for DevOps purpose
 # Test
 python -m unittest discover
 python -m unittest test.test_paramater_helper
+
+Discuss with Denny in [LinkedIn](https://www.linkedin.com/in/dennyzhang001) or [Blog](https://www.dennyzhang.com).
+
+Code is licensed under [MIT License](https://www.dennyzhang.com/wp-content/mit_license.txt).
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 ##-------------------------------------------------------------------
 ## @copyright 2017 DennyZhang.com
 ## Licensed under MIT 
-##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
 ##
 ## File : setup.py
-## Author : Denny <denny@dennyzhang.com>
+## Author : Denny <contact@dennyzhang.com>
 ## Description :
 ## --
 ## Created : <2017-01-24>
-## Updated: Time-stamp: <2017-01-27 11:37:31>
+## Updated: Time-stamp: <2017-09-07 21:35:01>
 ##-------------------------------------------------------------------
 from setuptools import setup
 
